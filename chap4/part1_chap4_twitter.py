76a56c89cb7b467ac19ea552fb5c00b411fd370e
Kim Do Ky <makingfunk0@gmail.com>
2017-07-15 01:05:21 +0900

    add chap4 twitter_1

---
+++ chap4/part1_chap4_twitter.py
@@ -0,0 +1,6 @@
+from twitter import *
+
+t = Twitter(aouth=OAuth([Access Token],[Access Token Secret],
+                        [Consumer Key],[Consumer Secret]))
+pythonTweets = t.search.tweets(q = "#python")
+print(pythonTweets)
