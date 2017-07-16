c6d434856bb135cfdf5135941d5c7dc0741503cc
Kim Do Ky <makingfunk0@gmail.com>
2017-07-17 00:43:52 +0900

    add part1 chap5 six degrees in mysql

---
+++ chap5/part1_chao5_3_5_six_degrees_testing.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import re
+import pymysql
+from urllib.request import urlopen
+
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='root', passwd=None, db='mysql', charset='utf8')
+
+cur = conn.cursor()
+cur.execute("USE wikipedia")
+
+def insertPageIfNotExists(url):
+    cur.execute("SELECT * FROM pages WHERE url = %s", (url))
+    if cur.rowcount == 0:
+        cur.execute("INSERT INTO pages (url) VALUES (%s)", (url))
+        conn.commit()
+        return cur.lastrowid
+    else:
+        return cur.fetchone()[0]
+
+def insertLink(fromPageId, toPageId):
+    cur.execute("SELECT * FROM links WHERE fromPageId = %s AND toPageId = %s", (int(fromPageId), int(toPageId)))
+    if cur.rowcount == 0:
+        cur.execute("INSERT INTO links (fromPageId, toPageId) VALUES (%s, %s)", (int(fromPageId), int(toPageId)))
+        conn.commit()
+
+pages = set()
+def getLinks(pageUrl, recursionLevel):
+    global pages
+    if recursionLevel > 4:
+        return;
+    pageId = insertPageIfNotExists(pageUrl)
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html, "html.parser")
+    for link in bsObj.find_all("a", href=re.compile("^(/wiki/)((?!:).)*$")):
+        insertLink(pageId, insertPageIfNotExists(link.attrs['href']))
+        if link.attrs['href'] not in pages:
+            # 새 페이지를 만났으니 추가하고 링크를 검색합니다.
+            newPage = link.attrs['href']
+            pages.add(newPage)
+            getLinks(newPage, recursionLevel+1)
+getLinks("/wiki/Kevin_Bacon", 0)
+cur.close()
+conn.close()
